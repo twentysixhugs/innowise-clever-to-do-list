a500b89c95895f4233d7aee1a3eb742214be2d6f
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-30 18:15:35 +0300

    Add UI for manipulating tasks

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -1,7 +1,9 @@
 import { Container } from "@mui/system";
+import { IconButton, Stack, Typography } from "@mui/material";
+import { Add } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 import { ITask } from "../../interfaces/task.interface";
 import { TaskEntry } from "../TaskEntry/TaskEntry";
-import { Stack, Typography } from "@mui/material";
 
 interface IOverviewProps {
   tasks: ITask[];
@@ -12,12 +14,35 @@ export const Overview = ({
   tasks,
   onIsTaskCompletedChange,
 }: IOverviewProps) => {
+  const navigate = useNavigate();
+
+  const handleTaskCreate = () => {
+    navigate("/new");
+  };
+
+  const handleTaskEdit = (id: string) => {
+    navigate(`/edit/${id}`);
+  };
+
+  const handleTaskDelete = (id: string) => {
+    // TODO: Process deletion (it's already confirmed if this handler is called)
+  };
+
   return (
     <Container sx={{ minHeight: "100vh" }}>
       <Stack spacing={3} marginTop={20}>
-        <Typography component="h1" variant="h2">
-          {tasks.length} tasks today
-        </Typography>
+        <Stack spacing={3} direction="row" alignItems="center">
+          <Typography component="h1" variant="h2">
+            {tasks.length} tasks today
+          </Typography>
+          <IconButton
+            onClick={handleTaskCreate}
+            sx={{ marginTop: "0.25rem !important" }}
+          >
+            <Add fontSize="large" />
+          </IconButton>
+        </Stack>
+
         {tasks.map(({ name, description, isCompleted, id }) => (
           <TaskEntry
             key={id}
@@ -26,6 +51,12 @@ export const Overview = ({
             isCompleted={isCompleted}
             onIsCompletedChange={() => {
               onIsTaskCompletedChange(id);
+            }}
+            onEdit={() => {
+              handleTaskEdit(id);
+            }}
+            onDelete={() => {
+              handleTaskDelete(id);
             }}
           />
         ))}
