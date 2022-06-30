4ef7441284cd6493f2667511dc0def1ca45e43a6
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-30 16:44:05 +0300

    Make rounded checkbox, add icon buttons for new card actions

--- src/components/TaskEntry/TaskEntry.tsx
+++ src/components/TaskEntry/TaskEntry.tsx
@@ -8,7 +8,13 @@ import {
   Checkbox,
   Stack,
 } from "@mui/material";
-import { ExpandMore as ExpandMoreIcon } from "@mui/icons-material";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  ExpandMore as ExpandMoreIcon,
+  CheckCircle,
+  CircleOutlined,
+} from "@mui/icons-material";
 import styled from "@emotion/styled";
 import { useState } from "react";
 
@@ -34,30 +40,40 @@ export const TaskEntry = ({
   return (
     <Card sx={{ padding: "1rem" }}>
       <Stack direction="row" alignItems="center">
-        <Checkbox onChange={onIsCompletedChange} checked={isCompleted} />
+        <Checkbox
+          onChange={onIsCompletedChange}
+          checked={isCompleted}
+          icon={<CircleOutlined />}
+          checkedIcon={<CheckCircle />}
+        />
         <Typography component="h2" variant="h5">
           {name}
         </Typography>
       </Stack>
-      {description && (
-        <>
-          <CardActions sx={{ padding: 0, "& button": { margin: 0 } }}>
-            <ExpandMore
-              expand={expanded}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </ExpandMore>
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <Typography component="p" variant="subtitle1" marginTop={1}>
-              {description}
-            </Typography>
-          </Collapse>
-        </>
-      )}
+
+      <CardActions sx={{ padding: 0 }}>
+        {description && (
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        )}
+        <IconButton>
+          <EditOutlined color="action" />
+        </IconButton>
+        <IconButton>
+          <DeleteOutlined color="action" />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Typography component="p" variant="subtitle1" marginTop={1}>
+          {description}
+        </Typography>
+      </Collapse>
     </Card>
   );
 };
@@ -71,6 +87,5 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
   return <IconButton {...other} />;
 })(({ expand }) => ({
   transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
   transition: "transform 0.2s ease-out",
 }));
