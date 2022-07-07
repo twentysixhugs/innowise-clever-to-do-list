d6b0ed175a65ea68bbd4efa9415a808d0d7ea8f6
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-07-07 11:51:46 +0300

    Reset selected date on signout

--- src/components/SignOut/SignOut.tsx
+++ src/components/SignOut/SignOut.tsx
@@ -1,15 +1,18 @@
 import styled from "@emotion/styled";
 import { getAuth, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { useSelectedDate } from "../../context/SelectedDateStore/SelectedDateStore";
 
 export const SignOut = () => {
   const navigate = useNavigate();
+  const { resetSelectedDate } = useSelectedDate();
 
   const handleClick: React.MouseEventHandler<HTMLButtonElement> = () => {
     const auth = getAuth();
 
     signOut(auth).then(() => {
       navigate("/signin");
+      resetSelectedDate();
     });
   };
 
