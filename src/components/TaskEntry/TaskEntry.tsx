d2e331df92afc4a2dc31b88b1d81035814907f77
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-07-13 09:37:33 +0300

    Show error toast on task actions errors

--- src/components/TaskEntry/TaskEntry.tsx
+++ src/components/TaskEntry/TaskEntry.tsx
@@ -15,12 +15,13 @@ import {
   CircleOutlined,
 } from "@mui/icons-material";
 import styled from "@emotion/styled";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTasks } from "../../context/TasksStore/TasksStore";
 import { StyledCardActions, StyledPaper } from "./TaskEntry.styles";
 import { TaskEntryProps } from "./TaskEntry.types";
 import { taskService } from "../../services/taskService";
+import { Toast } from "../Toast";
 
 export const TaskEntry = ({
   id,
@@ -30,6 +31,8 @@ export const TaskEntry = ({
 }: TaskEntryProps) => {
   const [expanded, setExpanded] = useState(false);
   const [confirmDeletion, setConfirmDeletion] = useState(false);
+
+  const [error, setError] = useState("");
 
   const navigate = useNavigate();
   const { toggleTaskCompletion, deleteTask } = useTasks();
@@ -53,14 +56,12 @@ export const TaskEntry = ({
     e
   ) => {
     setConfirmDeletion(false);
+    setError("");
     deleteTask(id);
 
     taskService.deleteOneForUser(id).catch((err) => {
-      console.log(err.message);
+      setError(err.message);
     });
-
-    // сделать отсюда запрос
-    // в then обновить контекст
   };
 
   /* Completion */
@@ -69,10 +70,12 @@ export const TaskEntry = ({
   ) => {
     toggleTaskCompletion(id);
 
+    setError("");
+
     taskService
       .updateOneForUser(id, { isCompleted: !isCompleted })
       .catch((err) => {
-        console.log(err.message);
+        setError(err.message);
       });
   };
 
@@ -81,66 +84,78 @@ export const TaskEntry = ({
     navigate(`/edit/${id}`);
   };
 
-  return (
-    <StyledPaper elevation={5}>
-      <Stack direction="row" alignItems="center">
-        <Checkbox
-          onChange={handleIsCompleteToggle}
-          checked={isCompleted}
-          icon={<CircleOutlined />}
-          checkedIcon={<CheckCircle />}
-        />
-        <Typography component="h2" variant="h5">
-          {name}
-        </Typography>
-      </Stack>
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
 
-      <StyledCardActions>
-        {description && (
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </ExpandMore>
-        )}
-        <IconButton onClick={handleEdit}>
-          <EditOutlined color="action" />
-        </IconButton>
-        <IconButton onClick={handleDeleteClick}>
-          <DeleteOutlined color="action" />
-        </IconButton>
-      </StyledCardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <Typography component="p" variant="subtitle1" marginTop={1}>
-          {description}
-        </Typography>
-      </Collapse>
-      <Collapse in={confirmDeletion} timeout="auto" unmountOnExit>
-        <Typography
-          component="p"
-          variant="subtitle1"
-          fontSize="1rem"
-          marginTop={2}
-        >
-          Are you sure you want to delete this task?
-        </Typography>
-        <Stack direction="row" spacing={2} marginTop={2}>
-          <Button
-            variant="contained"
-            color="error"
-            onClick={handleDeleteConfirm}
-          >
-            Delete
-          </Button>
-          <Button variant="contained" onClick={handleDeleteCancel}>
-            Cancel
-          </Button>
+  return (
+    <>
+      <Toast
+        color="error"
+        isOpen={!!error}
+        message={error}
+        onClose={() => setError("")}
+      />
+      <StyledPaper elevation={5}>
+        <Stack direction="row" alignItems="center">
+          <Checkbox
+            onChange={handleIsCompleteToggle}
+            checked={isCompleted}
+            icon={<CircleOutlined />}
+            checkedIcon={<CheckCircle />}
+          />
+          <Typography component="h2" variant="h5">
+            {name}
+          </Typography>
         </Stack>
-      </Collapse>
-    </StyledPaper>
+
+        <StyledCardActions>
+          {description && (
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          )}
+          <IconButton onClick={handleEdit}>
+            <EditOutlined color="action" />
+          </IconButton>
+          <IconButton onClick={handleDeleteClick}>
+            <DeleteOutlined color="action" />
+          </IconButton>
+        </StyledCardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <Typography component="p" variant="subtitle1" marginTop={1}>
+            {description}
+          </Typography>
+        </Collapse>
+        <Collapse in={confirmDeletion} timeout="auto" unmountOnExit>
+          <Typography
+            component="p"
+            variant="subtitle1"
+            fontSize="1rem"
+            marginTop={2}
+          >
+            Are you sure you want to delete this task?
+          </Typography>
+          <Stack direction="row" spacing={2} marginTop={2}>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={handleDeleteConfirm}
+            >
+              Delete
+            </Button>
+            <Button variant="contained" onClick={handleDeleteCancel}>
+              Cancel
+            </Button>
+          </Stack>
+        </Collapse>
+      </StyledPaper>
+    </>
   );
 };
 
