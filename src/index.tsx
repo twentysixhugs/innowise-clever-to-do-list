db1e0487d45969fd991782aa776a826008cc6d86
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-07-13 16:26:30 +0300

    Replace horizontal scroll fix causing side effects with CSS fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./components/App";
 import { initFirebase } from "./init/firebase";
-import { disableScrollOnMobiles } from "./init/disableScrollOnMobiles";
 
 initFirebase();
-disableScrollOnMobiles();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
