a9b3c95863a180cc935124f13f44f1c71a123e82
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-07-20 10:28:45 +0300

    Extract Sign Out styles to a separate file

---
+++ src/components/SignOut/SignOut.styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const StyledSignout = styled("button")({
+  fontSize: "1.2rem",
+  "&:hover": {
+    textDecoration: "underline",
+  },
+  color: "lightgrey",
+  background: "none",
+  cursor: "pointer",
+});
